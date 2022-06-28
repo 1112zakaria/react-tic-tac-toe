577dd760d2a4bb29152f5401aa95bdf51de9a0b4
1112zakaria <1112zakaria@gmail.com>
2022-06-28 20:21:40 +0000

    Added some button interrupt handlers and state

--- src/game.js
+++ src/game.js
@@ -1,22 +1,40 @@
 // Author: Zakaria Ismail
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button 
+        className="square" 
+        onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    //this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return <Square 
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}/>;
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
 
   render() {
